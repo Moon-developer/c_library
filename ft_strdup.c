514ac7bc79089589c3f069efef31f351fc8e01f8
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-16 08:49:22 +0200

    auto commit

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/09 11:24:56 by mafernan          #+#    #+#             */
-/*   Updated: 2017/08/17 15:49:40 by mafernan         ###   ########.fr       */
+/*   Updated: 2017/09/19 11:08:21 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,26 @@ char	*ft_strdup(const char *s1)
 	while (s1[i])
 	{
 		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup_notab(char *s1)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	if (!(dest = ft_strnew(ft_strlen(s1))))
+		return (NULL);
+	while (s1[i])
+	{
+		if (s1[i] == 9)
+			dest[i] = ' ';
+		else
+			dest[i] = s1[i];
 		i++;
 	}
 	dest[i] = '\0';
